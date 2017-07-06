bc266fab36a668d2cff253d8d156e278f56a1329
Verlon Smith <vsmith@AlteryxITs-MacBook-Pro-6.local>
2017-07-06 16:40:47 -0600

    Update Portal.elm file

--- QA_Portal/src/Portal.elm
+++ QA_Portal/src/Portal.elm
@@ -1,19 +1,19 @@
-module Portal exposing (onChange)
+module Portal exposing (onChange, add)
 
-import Html exposing (..)
-import Html.Events exposing (..)
-import Json.Decode exposing (..)
-import Msg exposing (..)
+import Html exposing (Html)
+import Html.Events
+import Json.Decode
+import Msg exposing (Msg)
 
 
 selectedTeamDecoder : Json.Decode.Decoder Msg
 selectedTeamDecoder =
-    Json.Decode.map SetSelectedTeam Html.Events.targetValue
+    Json.Decode.map Msg.SetSelectedTeam Html.Events.targetValue
 
 
 onChange : (String -> msg) -> Html.Attribute msg
 onChange tagger =
-    on "change" (Json.Decode.map tagger Html.Events.targetValue)
+    Html.Events.on "change" (Json.Decode.map tagger Html.Events.targetValue)
 
 
 add : number -> number -> number
